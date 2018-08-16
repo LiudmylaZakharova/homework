20db1fd88bfb5ee62dc24c082ab7a089f8171c77
LiudmylaZakharova <ludchen2009@gmail.com>
2018-08-16 16:54:05 +0300

    comment about first action

--- Homework 1.2.2.4.php
+++ Homework 1.2.2.4.php
@@ -4,7 +4,7 @@ $author = 'Oscar Wild';
 echo 'Simple single-quoted string with variable by $author' . PHP_EOL;
 echo 'Simple single-quoted string with escaping by \'' . $author . '\'' . PHP_EOL;
 echo "Simple double-quoted string with variable by {$author}" . PHP_EOL;
-echo "Double-quoted string with escape sequences\nbu '{$author}'" . PHP_EOL;
+echo "Double-quoted string with escape sequences\n by '{$author}'" . PHP_EOL;
 
 echo PHP_EOL;
 echo <<<HEREDOC
